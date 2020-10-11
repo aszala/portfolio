8b1a8a77ffbbb818f53bd6444b19847ed34dcb74
Abhay Zala <zala.abhay@gmail.com>
2020-10-11 03:33:56 -0400

    Designed Header

---
+++ config.js
@@ -0,0 +1,11 @@
+var firebaseConfig = {
+   apiKey: "AIzaSyDq9wo9Jdn4PDhbHPVg0btkYUVVwsCatdU",
+   authDomain: "portfolio-4722e.firebaseapp.com",
+   databaseURL: "https://portfolio-4722e.firebaseio.com",
+   projectId: "portfolio-4722e",
+   storageBucket: "portfolio-4722e.appspot.com",
+   messagingSenderId: "344412709404",
+   appId: "1:344412709404:web:efc32e40ea3ab2ccd375d9"
+ };
+ 
+ firebase.initializeApp(firebaseConfig);
