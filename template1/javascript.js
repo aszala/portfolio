f08b0bc00e7026d66df4127d2191a32955d0613a
Abhay Zala <zala.abhay@gmail.com>
2021-01-31 05:34:35 -0500

    Add files via upload

--- template1/javascript.js
+++ template1/javascript.js
@@ -7,11 +7,22 @@ $(function() {
 			$("#menu-icon").addClass("fa-bars");
 			$("#menu-icon").removeClass("fa-close");
 		} else {
-			$("#nav-menu").css("height", "150px");
+			$("#nav-menu").css("height", "200px");
 			$("#menu-icon").addClass("fa-close");
 			$("#menu-icon").removeClass("fa-bars");
 		}
 
 		navOpen = !navOpen;
 	});
+});
+
+let topofDiv = $("#header").offset().top;
+let height = $("#header").outerHeight();
+
+$(window).scroll(function() {
+    if ($(window).scrollTop() > (topofDiv + height - 50)) {
+       $("#menu-container").css("background", "rgb(30, 30, 30)");
+    } else {
+       $("#menu-container").css("background", "transparent");
+    }
 });
