a7ad6df47f9d4c2d9f4a03a726abe200e5a8b50e
Abhay Zala <zala.abhay@gmail.com>
2021-10-29 15:39:35 -0400

    Setup

--- src/App.svelte
+++ src/App.svelte
@@ -1,30 +1,19 @@
 <script>
-	export let name;
+import router from 'page';
+import Home from './components/Home.svelte'
+
+let page;
+let title;
+
+router('/', () => { page = Home; title = "Abhay Zala Profolio"; });
+// router('/colloquium', () => { page = Colloquium; title = "Colloquium Series"; });
+router.start()
+
 </script>
 
-<main>
-	<h1>Hello {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
-</main>
+<svelte:head>
+	<title>{title}</title>
+	<script async defer src="https://platform.twitter.com/widgets.js"></script>
+</svelte:head>
 
-<style>
-	main {
-		text-align: center;
-		padding: 1em;
-		max-width: 240px;
-		margin: 0 auto;
-	}
-
-	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
-		font-size: 4em;
-		font-weight: 100;
-	}
-
-	@media (min-width: 640px) {
-		main {
-			max-width: none;
-		}
-	}
-</style>
+<svelte:component this="{page}" />
